753d0309511e1706ca2b9520ec2b39ee84c3c044
LifeofAGeek <sethi8001@gmail.com>
2020-04-01 20:37:22 +0530

    using string.h

--- DSA Learning Series/Series-1/MULTHREE.cpp
+++ DSA Learning Series/Series-1/MULTHREE.cpp
@@ -1,6 +1,19 @@
 #include<bits/stdc++.h>
-#include<string>
 using namespace std;
+#define MOD 10000000000007
+
+long long fast_power(long long base, long long power) {
+    long long result = 1;
+    while(power > 0) {
+
+        if(power % 2 == 1) { // Can also use (power & 1) to make code even faster
+            result = (result*base) % MOD;
+        }
+        base = (base * base) % MOD;
+        power = power / 2; // Can also use power >>= 1; to make code even faster
+    }
+    return result;
+}
 
 int main()
 {
@@ -12,14 +25,13 @@ int main()
         cin>>d0;
         cin>>d1;
         int sum=d0+d1,num;
-        long ans=d0*pow(10,--k) + d1*pow(10,--k);
+        long long ans=d0 * fast_power(10,--k)+ d1 * fast_power(10,--k);
         while (k)
         {
             num=sum%10;
             sum+=num;
-            ans=ans+num*pow(10,--k);
+            ans+=num*pow(10,--k);
         }
-        cout<<ans<<endl;
         if(ans%3==0) cout<<"YES";
         else cout<<"NO";
     }
